3b2dca1e8c50cf2d966cdd06758d532fffd478d6
Priyanka Ghansela <priyanka.ghanselaa@gmail.com>
2023-09-13 01:41:08 +0530

    Added host

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.api.v1.main import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0",  port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
